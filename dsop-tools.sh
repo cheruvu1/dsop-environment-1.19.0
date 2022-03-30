3fde3ddcb51dcd81281bba234ef0f8ba3ab4e8b7
cheruvu1 <80857514+cheruvu1@users.noreply.github.com>
2022-03-30 16:15:32 -0500

    Update dsop-tools.sh

--- dsop-tools.sh
+++ dsop-tools.sh
@@ -10,13 +10,13 @@ wget https://releases.hashicorp.com/terraform/1.1.7/terraform_1.1.7_linux_amd64.
 sudo apt-get install zip -y
 unzip terraform*.zip
 sudo mv terraform /usr/local/bin
-terraform version
+
 
 
 # JQ
 sudo apt-get update 
 sudo apt install jq
-jq --version
+
 
 
 # Kubectl
@@ -28,34 +28,43 @@ sudo chmod +x kubectl
 sudo mkdir -p ~/.local/bin/kubectl
 sudo mv ./kubectl ~/.local/bin/kubectl
 # and then add ~/.local/bin/kubectl to $PATH
-kubectl version
+
 
 
 # Azure CLI
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
-az version
+
 
 # gpg
 sudo apt-get install -y gpg
-gpg --version
+
 
 
 # Sops
 wget https://github.com/mozilla/sops/releases/download/v3.7.1/sops_3.7.1_amd64.deb
 sudo dpkg -i sops_3.7.1_amd64.deb
-sops --version 
+
 
 # Kustomize
 sudo curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh" | bash
 sudo mv kustomize /usr/local/bin
-kustomize version
+
 
 # Flux
 curl -s https://fluxcd.io/install.sh | sudo bash
-flux --version
+
 
 # Install Helm3 
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
 
+
+terraform version
+jq --version
+kubectl version
+flux --version
+kustomize version
+sops --version 
+gpg --version
+az version
